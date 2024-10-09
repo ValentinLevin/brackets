409f79f79b7e8c1197aff759782683e9f5bdbc09
Valentin Levin <levin_valentin@mail.ru>
2024-10-09 17:16:55 +0500

    feat: brackets compelet

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = bracketsConfig.map(item => item[0] + item[1]);
+    let found;
+    let index;
+    let temp = str;
+    do {
+        found = false;
+        for (let i = 0; i < brackets.length; i += 1) {
+            index = temp.indexOf(brackets[i]);
+            if (index !== -1) {
+                temp = temp.replace(brackets[i], '');
+                found = true;
+            }
+        }
+    } while (temp.length > 0 && found);
+
+    return temp.length === 0;
 }
